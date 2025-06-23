f9da04f301c7c57406716c6bc3295a74ec3193c7
Myron R Schippers Jr <dubtwee@gmail.com>
2025-06-23 12:22:47 -0400

    comlpleting lesson follow for v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { ref } from 'vue';
+import socksGreenImage from './assets/images/socks_green.jpeg';
 
 const product = ref('Socks');
 const description = ref(
   'These are outstanding socks. Made from first rate alpaca fur.'
 );
+
+// Attribute binding with `v-bind`
+// - without `ref` `v-bind` is not dynamic
+const image = ref(socksGreenImage);
 </script>
 
 <template>
@@ -12,7 +17,9 @@ const description = ref(
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <!-- image goes here -->
+        <img v-bind:src="image" />
+        <!-- Shorthand for `v-bind:` -->
+        <!-- <img :src="image" /> -->
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
