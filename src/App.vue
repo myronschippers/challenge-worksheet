e5c698ab3efb80df703ba579e526075443204749
Myron R Schippers Jr <dubtwee@gmail.com>
2025-06-23 12:10:32 -0400

    adding refs for vue content

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const product = ref('Socks');
+const description = ref(
+  'These are outstanding socks. Made from first rate alpaca fur.'
+);
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
+        <p>{{ description }}</p>
       </div>
     </div>
   </div>
